a26bb09d4dc9a757cd97c4321079791d4e3f05e6
meyyh <cooperrs123@gmail.com>
2023-12-19 16:43:26 -0600

    commit

---
+++ wayapp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <wayland-client.hpp>
+
+
+int
+main(int argc, char *argv[])
+{
+    struct wl_display *display = wl_display_connect(NULL);
+    if (!display) {
+        fprintf(stderr, "Failed to connect to Wayland display.\n");
+        return 1;
+    }
+    fprintf(stderr, "Connection established!\n");
+
+    wl_display_disconnect(display);
+    return 0;
+}
